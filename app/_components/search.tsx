543bb74bd8823b27215e7e434a9f8090674dde07
plainp <tarantoj04@gmail.com>
2024-04-30 21:07:10 -0300

    feat: add initial search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,22 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const Search = () => {
+    return ( 
+        <div className="flex gap-4">
+            <Input placeholder="Buscar restaurantes" className="border-none"/>
+            <Button size="icon">
+                <SearchIcon size={20}/>
+            </Button>
+        </div>
+     );
+}
+ 
+export default Search;
+<div className="flex gap-4">
+    <Input />
+    <Button size="icon">
+        <SearchIcon size={20}/>
+    </Button>
+</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="pt-6 px-5">
+        <Search />
+      </div>
+    </>
   );
 }
